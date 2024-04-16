ec6414f123c8cfe628613cef3001f6a8abfe23ba
Daniel Dmitry Shalimov <tun63930@temple.edu>
2024-04-16 14:54:49 -0400

    djusted login form size so that its easier to navigate

--- BookStoreSystem/View/frmLogin.Designer.cs
+++ BookStoreSystem/View/frmLogin.Designer.cs
@@ -163,7 +163,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.ClientSize = new System.Drawing.Size(736, 366);
             this.Controls.Add(this.btnTemp);
             this.Controls.Add(this.gbAccountType);
             this.Controls.Add(this.btnSignUp);
