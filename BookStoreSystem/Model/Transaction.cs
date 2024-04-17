687dac58f257b2add12f2cbdcac415fe9839fd10
Daniel Dmitry Shalimov <tun63930@temple.edu>
2024-04-16 20:05:17 -0400

    Created transaction class

--- BookStoreSystem/Model/Transaction.cs
+++ BookStoreSystem/Model/Transaction.cs
@@ -6,7 +6,51 @@ using System.Threading.Tasks;
 
 namespace BookStoreSystem
 {
-    internal class Transaction
+    public  class Transaction
     {
+        private int transactionID;
+        private int userID;
+        private int bookID;
+        private DateTime purchaseDate;
+        private double bookCost;
+
+        public Transaction(int transactionID, int userID, int bookID, DateTime purchaseDate, double bookCost)
+        {
+            this.transactionID = transactionID;
+            this.userID = userID;
+            this.bookID = bookID;
+            this.purchaseDate = purchaseDate;
+            this.bookCost = bookCost;
+        }
+
+        public int TransactionID
+        {
+            get { return transactionID; }
+            set { transactionID = value; }
+        }
+
+        public int UserID
+        {
+            get { return userID; }
+            set { userID = value; }
+        }
+
+        public int BookID
+        {
+            get { return bookID; }
+            set { bookID = value; }
+        }
+
+        public DateTime PurchaseDate
+        {
+            get { return purchaseDate; }
+            set { purchaseDate = value; }
+        }
+
+        public double BookCost
+        {
+            get { return bookCost; }
+            set { bookCost = value; }
+        }
     }
 }
