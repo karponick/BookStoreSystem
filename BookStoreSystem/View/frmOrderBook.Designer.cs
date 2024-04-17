312b1850396eb2d9acdfeb4b2916a494f2aa5e0d
Daniel Dmitry Shalimov <tun63930@temple.edu>
2024-04-17 11:50:10 -0400

    Added Flow Layout panels to book order form

--- BookStoreSystem/View/frmOrderBook.Designer.cs
+++ BookStoreSystem/View/frmOrderBook.Designer.cs
@@ -29,12 +29,42 @@ namespace BookStoreSystem.View
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.flpCustomersWhoOrdered = new System.Windows.Forms.FlowLayoutPanel();
+            this.flpListOfBooks = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // flpCustomersWhoOrdered
+            // 
+            this.flpCustomersWhoOrdered.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flpCustomersWhoOrdered.Location = new System.Drawing.Point(966, 268);
+            this.flpCustomersWhoOrdered.Name = "flpCustomersWhoOrdered";
+            this.flpCustomersWhoOrdered.Size = new System.Drawing.Size(333, 349);
+            this.flpCustomersWhoOrdered.TabIndex = 14;
+            // 
+            // flpListOfBooks
+            // 
+            this.flpListOfBooks.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flpListOfBooks.Location = new System.Drawing.Point(54, 268);
+            this.flpListOfBooks.Name = "flpListOfBooks";
+            this.flpListOfBooks.Size = new System.Drawing.Size(834, 349);
+            this.flpListOfBooks.TabIndex = 13;
+            // 
+            // frmOrderBook
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1395, 669);
+            this.Controls.Add(this.flpCustomersWhoOrdered);
+            this.Controls.Add(this.flpListOfBooks);
+            this.Name = "frmOrderBook";
             this.Text = "frmOrderBook";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flpCustomersWhoOrdered;
+        private System.Windows.Forms.FlowLayoutPanel flpListOfBooks;
     }
 }
